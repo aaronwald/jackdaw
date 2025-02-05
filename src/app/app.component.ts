929e12a1c311db4d968a857f2b47ab1e4636d61a
Aaron Wald <aaronwald@gmail.com>
2025-02-05 14:24:11 +0000

    d

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,31 @@ import { selectRook } from './rook.selector';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
+
 @Component({
   selector: 'app-root',
   providers: [RookService,],
-  imports: [CommonModule, MatSlideToggleModule, MatToolbarModule, MatButtonModule],
+  imports: [CommonModule, MatSlideToggleModule, MatToolbarModule, MatButtonModule, MatGridListModule],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit, OnDestroy {
+  tiles: Tile[] = [
+    { text: 'One', cols: 3, rows: 1, color: 'magenta' },
+    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
+    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
+  ];
   active = 1;
 
   title = 'jackdaw';
